438d989ce47d8da07de6c669e5337d41986c894d
Zetax <ernzerhofffinn@gmail.com>
2024-10-08 18:53:03 +0200

    Switching to vuepress

---
+++ docs/.vuepress/.temp/prismjs/config.js
@@ -0,0 +1,12 @@
+import "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/highlighter-helper/lib/client/styles/base.css"
+import "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+plugin-prismjs@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/plugin-prismjs/lib/client/styles/nord.css"
+import "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/highlighter-helper/lib/client/styles/line-numbers.css"
+import "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/highlighter-helper/lib/client/styles/notation-highlight.css"
+import "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/highlighter-helper/lib/client/styles/collapsed-lines.css"
+import { setupCollapsedLines } from "C:/Users/finni/Desktop/Athena/Wiki/node_modules/.pnpm/@vuepress+highlighter-helper@2.0.0-rc.52_@vueuse+core@11.1.0_vuepress@2.0.0-rc.17/node_modules/@vuepress/highlighter-helper/lib/client/composables/collapsedLines.js"
+
+export default {
+  setup() {
+    setupCollapsedLines()
+  }
+}
